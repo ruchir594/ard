fb17168846f627242c95bea411a79fee6d5f029c
ruchir594 <ruchir594@gmail.com>
2018-03-21 15:27:57 -0400

    combined 1,2,5,6,7

--- pyrig/gesture.py
+++ pyrig/gesture.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from descent import Descent
+from collections import Counter
 
 class Gesture(object):
 
@@ -45,34 +46,47 @@ class Gesture(object):
                 y.append(1)
         return y
 
-    def gd_touch(self, data, threshold, control=0):
+    def gd_double_touch(self, data, threshold, control=0):
         i=0
-        temp=-1
+        temp1 = -1
+        temp2 = -1
         y=[]
 
         last_i = -1;
 
         while i < len(data)-1:
-            temp = self.obj.gradient_descent(data[i], threshold)
-            while temp == -1 and i < len(data)-1:
+            temp1 = self.obj.gradient_descent(data[i], threshold)
+            temp2 = self.obj.gradient_descent(data[i][::-1], threshold)
+            while temp1 == -1 and i < len(data)-1:
                 i+=1
-                temp = self.obj.gradient_descent(data[i], threshold)
+                temp1 = self.obj.gradient_descent(data[i], threshold)
+                temp2 = self.obj.gradient_descent(data[i][::-1], threshold)
 
-            w=[]
-            temp = self.obj.gradient_descent(data[i], threshold)
-            while temp != -1 and i < len(data)-1:
-                w.append(temp)
+            w1=[]
+            w2=[]
+            temp1 = self.obj.gradient_descent(data[i], threshold)
+            temp2 = self.obj.gradient_descent(data[i][::-1], threshold)
+            while temp1 != -1 and i < len(data)-1:
+                w1.append(temp1)
+                w2.append(15-temp2)
                 i+=1
-                temp = self.obj.gradient_descent(data[i], threshold)
+                temp1 = self.obj.gradient_descent(data[i], threshold)
+                temp2 = self.obj.gradient_descent(data[i][::-1], threshold)
 
             # now w has one chunk
+            print len(w1), len(w2), w1, w2
 
-            if len(w) > 2:
-                if i-last_i < 20:
-                    y.pop()
-                    y.append('d_'+str(max(w)))
+            if len(w1) > 2:
+                if w1 == w2:
+                    if i-last_i < 20:
+                        y.pop()
+                        y.append('d_'+str(max(k for k,v in Counter(w1).items() if v>1)))
+                    else:
+                        y.append(max(w1))
                 else:
-                    y.append(max(w))
+                    if i-last_i < 20:
+                        y.pop()
+                    y.append('m_'+str(max(k for k,v in Counter(w1).items() if v>1))+'_'+str(max(k for k,v in Counter(w2).items() if v>1)))
 
             last_i = i
         return y
--- pyrig/predict.py
+++ pyrig/predict.py
@@ -1,6 +1,6 @@
 # read from file
 
-with open('../data/05/glasses/raw_6_0_unsync.txt') as f:
+with open('../data/05/glasses/raw_7_0_unsync.txt') as f:
     x = f.readlines()
 
 data=[]
@@ -21,11 +21,11 @@ swipe_threshold = int(d_obj.median(data[0]))*0.75
 touch_threshold = int(d_obj.median(data[0]))*0.10
 
 y = g_obj.gd(data, swipe_threshold)
-if y:
+if len(y)>3:
     print '~~~~\nSwipe Detected, Swipe gesture direction\n'
     print len(y), y
 else:
-    y = g_obj.gd_touch(data, touch_threshold, 0)
+    y = g_obj.gd_double_touch(data, touch_threshold, 0)
     if y:
         print '~~~~\nTouch Detected, index of touch\n'
         print len(y), y
